bc464d631d18258c8385e37cd2d8915e23ffb000
jflo846 <floden.jennie@gmail.com>
2020-04-16 13:51:00 +0200

    lagt in metoder

--- www/Game.js
+++ www/Game.js
@@ -1,4 +1,18 @@
-class Game { }
+class Game {
 
+  constructor() {
+
+  }
+
+  start() { }
+  
+  tellTurn(player) { }
+  
+  over(won) { }
+  
+  addEventListener() { }
+  
+  
+}
 // make it possible to test on backend
 if (typeof global !== 'undefined') { global.Game = Game };
