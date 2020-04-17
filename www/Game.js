3d2db138d2396733167276d3cd540b1f6a7d7180
jflo846 <floden.jennie@gmail.com>
2020-04-17 11:54:01 +0200

    game-constructor-kod

--- www/Game.js
+++ www/Game.js
@@ -1,18 +1,18 @@
 module.exports = class Game {
 
   constructor() {
-
+    
+    this.addEventListener();
+    this.start();
   }
 
   start() { }
-  
+
   tellTurn(player) { }
   
   over(won) { }
   
   addEventListener() { }
-  
-
 }
 // make it possible to test on backend
 if (typeof global !== 'undefined') { global.Game = Game };
