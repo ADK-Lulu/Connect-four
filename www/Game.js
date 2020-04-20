e447476297deb41632ae4ac48b36496b21339509
jflo846 <floden.jennie@gmail.com>
2020-04-20 06:32:20 +0200

    game-tellTurn kod

--- www/Game.js
+++ www/Game.js
@@ -12,7 +12,20 @@ module.exports = class Game {
   }
   
 
-  tellTurn(player) { }
+  tellTurn(player) { 
+    let $message = document.querySelector('.message');
+    
+    if (player !== 1 || player !== 2) {
+      return 'Player must be 1 or 2';
+    }
+    if (player === 1) {
+      return document.querySelector('.message').innerHTML = 'Röds tur';
+    }
+    if (player === 2) {
+      return document.querySelector('.message').innerHTML = 'Guls tur';
+    }
+       
+  }
 
   over(won) { }
 
