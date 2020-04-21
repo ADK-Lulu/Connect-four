efb44e88bd3979d1515fd3726770581bf986c07d
ADK-Lulu <ulrika.alehag@gmail.com>
2020-04-21 11:44:18 +0200

    fixade spelplanen i render()

--- www/Board.js
+++ www/Board.js
@@ -5,7 +5,7 @@ class Board {
       throw (new Error("game must be an instance of Game"))
     }
     this.game = game;
-    let matrix = [
+    this.matrix = [
       [0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0],
@@ -21,12 +21,24 @@ class Board {
   }
   async makeMove(column) {
 
-
   }
 
   winCheck() { }
 
-  render() { }
+  render() {
+    //Tar och loopar igenom matrix, en array av arrayer, som blir spelplanen
+    for (let element of this.matrix) {
+      //Loopar igenom arrayerna var för sig i matrix.
+      for (let $childElement of element) {
+        //Hittar det första elementet med klassen board
+        $childElement = $('.board');
+        let $childElement2 = document.createElement('div');
+        $childElement2.innerHTML = `<div></div>`;
+        $childElement.append($childElement2);
+      }
+    }
+
+  }
 
   markWin(combo) { }
 
