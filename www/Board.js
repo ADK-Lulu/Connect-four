55e7e104cc0109e796ccbef6f71cd8c1b1ff017a
teknikMarit <teknikMarit@gmail.com>
2020-04-23 21:47:43 +0200

    blandat i makeMove(), mest koll om col är full

--- www/Board.js
+++ www/Board.js
@@ -13,26 +13,47 @@ class Board {
       [0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0]
     ];
-    let currentPlayer = 1;
-    let playInProgress = false;
+    this.currentPlayer = 1;
+    this.playInProgress = false;
     this.addEventListener();
-    this.removeEventListener();
     this.render();
-    game.tellTurn(currentPlayer);
+    game.tellTurn(this.currentPlayer);
 
   }
+
   async makeMove(column) {
-    if (!Number.isInteger(column) || column > 0 || column < 6) {
+
+    if (!Number.isInteger(column) || column < 0 || column > 6) {
+
       throw (new Error('column must be an integer between 0 and 6'))
     }
     else if (this.playInProgress === true) {
       return null;
     }
-    else if (meh /*column is full*/) {
-      //loopa igenom olika lösningar? 
-      //Skriv kod här för att kolla om column är full
-      return false;
+
+    //plattar ut matrixen för att sedan i switchen kolla om det finns någon 0, dvs en vit/tom plats, i aktuell column,
+    //om det inte finns så returneras false
+
+    let flatMatrix = this.matrix.flat();
+
+    switch (column) {
+
+      case 0: if (flatMatrix.filter((item, index) => index % 7 === 0).indexOf(0) === -1) { return false };
+        break;
+      case 1: if (flatMatrix.filter((item, index) => index % 7 === 1).indexOf(0) === -1) { return false };
+        break;
+      case 2: if (flatMatrix.filter((item, index) => index % 7 === 2).indexOf(0) === -1) { return false };
+        break;
+      case 3: if (flatMatrix.filter((item, index) => index % 7 === 3).indexOf(0) === -1) { return false };
+        break;
+      case 4: if (flatMatrix.filter((item, index) => index % 7 === 4).indexOf(0) === -1) { return false };
+        break;
+      case 5: if (flatMatrix.filter((item, index) => index % 7 === 5).indexOf(0) === -1) { return false };
+        break;
+      case 6: if (flatMatrix.filter((item, index) => index % 7 === 6).indexOf(0) === -1) { return false };
+        break;
     }
+
     this.playInProgress = true;
     //Sätt brickan högst upp
     //this.render();
@@ -40,17 +61,16 @@ class Board {
     //Anropa sleep 
     //om det går, börja om igen från anropa render 
     //anropa this.winCheck, om den anropar något true; 
-    //anropa this.removeEventListener();
+    // this.removeEventListener();
     //om winCheck returnerar något med combo ska metoden this.markWin(combo) anropas 
     //game.over(winner)
     //returnera true
-    //Byt spelare 
-    if (player === 1) {
-      player = 2;
-    } else if (player === 2) {
-      player = 1;
-    }
-    this.game.tellTurn(currentPlayer);
+
+    //Byt spelare
+    this.currentPlayer === 1 ? this.currentPlayer = 2
+      : this.currentPlayer = 1;
+
+    this.game.tellTurn(this.currentPlayer);
     this.playInProgress = false;
     return true;
 
@@ -75,8 +95,8 @@ class Board {
   markWin(combo) { }
 
   /*lyssnar efter click-händelser till elementet med css-klassen board i DOM:en.
-Listar ut index för columner och skickar till makeMove. 
-*/
+  Listar ut index för columner och skickar till makeMove. 
+  */
   addEventListener() {
 
     this.listener = event => {
@@ -86,7 +106,7 @@ Listar ut index för columner och skickar till makeMove.
       let index = $allClicked.indexOf($clicked);
       let column = index % 7;
       this.makeMove(column);
-      console.log(column)
+
     }
     $('.board').addEventListener('click', this.listener);
   }
