9b72345bc60dbfe67a68ba8a2e1edab430b9a464
ADK-Lulu <ulrika.alehag@gmail.com>
2020-04-23 11:09:19 +0200

    Pushar senaste versionen och step-definitions

--- step-definitions/play-game.js
+++ step-definitions/play-game.js
@@ -5,7 +5,7 @@ require('./_include-all')();
 module.exports = function () {
 
   let currentPlayer;
-  
+
   let game = new Game();
   let board = new Board(game);
 
@@ -82,13 +82,13 @@ module.exports = function () {
   this.When(/^matrix should be set to an array of (\d+) elements$/, function (rows) {
     expect(board.matrix.length).to.equal(+rows);
   });
-  
+
   this.When(/^each element should be set to a array of (\d+) elements$/, function (column) {
     for (let column of board.matrix) {
       column = board.matrix[0].length
       expect(column).to.equal(column);
     }
-     
+
   });
 
   this.When(/^each element should have the value of (\d+)$/, function (element) {
@@ -109,12 +109,57 @@ module.exports = function () {
 
   this.Then(/^the method should call addEventListener\(\) and render\(\)$/, function () {
     expect(() => game.addEventListener(), board.render())
-    
+
   });
 
   this.Then(/^it should call tellTurn\(\) with currentPlayer as a argument$/, function () {
-    currentPlayer = 1; 
-    expect(()=>game.tellTurn(currentPlayer))
+    currentPlayer = 1;
+    expect(() => game.tellTurn(currentPlayer))
+  });
+
+  this.Given(/^that the argument won is not draw$/, function () {
+    expect(() => game.over(won)).to.not.equal("draw");
+  });
+
+  this.Given(/^that the argument won is not (\d+)$/, function (player) {
+    expect(() => game.over(won)).to.not.equal(player);
+  });
+
+  //Måste fixa i scenario till scenario outline? 
+  this.Then(/^the error 'won must be "([^"]*)", (\d+) or (\d+)' will be thrown$/, function (draw, player1, player2) {
+    expect(draw).to.equal("draw");
+    expect(player1).to.equal(1);
+    expect(player2).to.equal(2);
+
+  });
+
+  this.Given(/^that the argument won is draw$/, function () {
+    expect(game.over(won)).to.equal("draw");
+
+  });
+
+  this.Given(/^that the argument won is (\d+)$/, function (player) {
+    expect(game.over(won)).to.equal(player);
+  });
+
+  this.Then(/^the message "([^"]*)" is shown$/, function (message) {
+    expect().to.equal(message);
+  });
+
+  this.Given(/^that the game is over$/, function () {
+    expect(game.over(won)).to.be.true;
+  });
+
+  this.Given(/^that there is a button in the \.message element with the class \.again$/, function () {
+
+  });
+
+  this.When(/^I press the button$/, function () {
+
+  });
+
+  this.Then(/^the game should restart$/, function () {
+
   });
 
 
