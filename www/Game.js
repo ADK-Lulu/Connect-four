ad2907ed5b3088e55297753af427c9361d1d4660
ADK-Lulu <ulrika.alehag@gmail.com>
2020-04-29 14:27:24 +0200

    Ulrika och Marit programmerade in VG-krav-tillägg

--- www/Game.js
+++ www/Game.js
@@ -10,22 +10,33 @@ class Game {
 
     this.player1 = prompt('Vad heter spelare 1?');
     if (this.player1 === '' || this.player1 === null) { this.player1 = 'Röd' }
+
     this.player2 = prompt('Vad heter spelare 2?');
     if (this.player2 === '' || this.player2 === null) { this.player2 = 'Gul' }
+
     this.board = new Board(this);
 
   }
 
-
   tellTurn(player) {
     let $message = $('.message');
+    let playerWithoutS;
+    let playerTwoWithoutS;
 
     if (player !== 1 && player !== 2) {
       throw (new Error('player must be 1 or 2'));
     }
 
-    $message.innerHTML = player === 1 ? `${this.player1}s tur...`
-      : player === 2 ? `${this.player2}s tur...`
+    if ([...this.player1].pop() === 's') {
+      playerWithoutS = this.player1.slice(0, -1)
+    } else { playerWithoutS = this.player1; }
+
+    if ([...this.player2].pop() === 's') {
+      playerTwoWithoutS = this.player2.slice(0, -1)
+    } else { playerTwoWithoutS = this.player2; }
+
+    $message.innerHTML = player === 1 ? `${playerWithoutS}s tur...`
+      : player === 2 ? `${playerTwoWithoutS}s tur...`
         : "";
   }
 
