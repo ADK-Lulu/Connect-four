d2fb76752b42f6aca08e1366959cd7555315abba
ADK-Lulu <ulrika.alehag@gmail.com>
2020-05-04 11:26:21 +0200

    AddEventListener, board and game

--- step-definitions/game-addEventListener.js
+++ step-definitions/game-addEventListener.js
@@ -4,4 +4,20 @@ require('./_include-all')();
 
 module.exports = function () {
 
+  let addEventListenerWasCalled = false;
+
+  class TestGame extends Game {
+
+    addEventListener() {
+      addEventListenerWasCalled = true;
+    }
+  }
+
+  let game = new TestGame();
+
+  this.Given(/^that the method addEventListener for game was called$/, function () {
+    game.addEventListener();
+    expect(addEventListenerWasCalled).to.be.true;
+  });
+
 }
