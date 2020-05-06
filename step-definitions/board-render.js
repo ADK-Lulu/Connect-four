8965e13a2e780b986b923b365f321d30638bc516
teknikMarit <teknikMarit@gmail.com>
2020-05-06 15:03:26 +0200

    justerat render

--- step-definitions/board-render.js
+++ step-definitions/board-render.js
@@ -7,27 +7,22 @@ module.exports = function () {
   let currentPlayer;
   let game = new Game();
   let board;
+  let nodeBoard;
 
-  //Scenario: Render shall change color on the div-elements in DOM depending on which player is the current one
   this.When(/^(\d+) is the value of the currentPlayer$/, function (value) {
     currentPlayer = +value;
   });
 
-  let nodeBoard;
   this.When(/^the currentPlayer has made a move and placed a disc on index (\d+) and (\d+)$/, function (row, col) {
     board = new Board(game)
-    board.matrix[+row][+col] = currentPlayer; //sätter gul/röd plupp på vald position
+    board.matrix[+row][+col] = currentPlayer;
     board.render()
     nodeBoard = [...$$('.board > div')][+col];
-
   });
 
   this.Then(/^"([^"]*)" will be the chosen color on the div by help from the css\-class board$/, function (color) {
-    //Kollar att rgbvärdet är rätt
     let expectedColor = nodeBoard.className.includes(color)
-
     expect(expectedColor, 'Wrong color displayed on the disc').to.be.true;
-
   });
 
   this.When(/^the matrix is rendered it should have empty div elements inside of a div$/, function () {
